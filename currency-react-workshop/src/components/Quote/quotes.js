daa6eca1f99e3e05331f1e0dbaf8b251289b2caa
Semion Krapivin <s4mm4el@Semions-MacBook-Pro.local>
2022-10-08 08:32:24 +0200

    feat: add quotes reference file

---
+++ currency-react-workshop/src/components/Quote/quotes.js
@@ -0,0 +1,14 @@
+export const quotes = {
+    {
+        text: "Работа не волк - в лес не убежит";
+        author: "Народная пословица";
+    };
+    {
+        text: "Откуплюсь по 41 через месяц";
+        author: "Илья Варламов";
+    };
+    {
+        text: "Wabadabalabwb";
+        author: "Rick Sanchez";
+    };
+}
