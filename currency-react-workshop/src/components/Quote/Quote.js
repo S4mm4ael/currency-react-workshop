c27340d131b318b32732e9018302626cf0742dfa
Semion Krapivin <s4mm4el@Semions-MacBook-Pro.local>
2022-10-08 08:26:30 +0200

    feat: add Quote js

---
+++ currency-react-workshop/src/components/Quote/Quote.js
@@ -0,0 +1,12 @@
+import quotes from './quotes'
+import styles from './Quote.module.css'
+
+export function Quote(){
+    const randomIndex= Math.floor(Math.random() * quotes.length);
+    const {author, text} = quotes[randomIndex];
+
+    return (<footer className={styles.quote}>
+    <blockquote>{text}</blockquote>;
+    <cite className={styles.author}>- {author}</cite>);
+    </footer>);
+}
