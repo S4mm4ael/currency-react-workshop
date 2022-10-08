a68529c0d21792c1ee232772a810f959a60057fb
Semion Krapivin <s4mm4el@Semions-MacBook-Pro.local>
2022-10-08 08:27:34 +0200

    feat: add Quotes trst and styles

---
+++ currency-react-workshop/src/components/Quote/Quote.test.js
@@ -0,0 +1,26 @@
+import {render} from "@testing-library/react";
+import {Quote} from "./Quote";
+import quotes from "./quotes"
+
+beforeEach(()=>{
+    jest.spyOn(global.Math, "random").mockReturnValue(0.42);
+});
+
+afterEach(()=> {
+    jest.restoreAllMocks();
+})
+
+describe('when rendered', () => {
+    const {text, author} = quotes[1]
+    it('should contain an expected text', ()=> {
+        render(<Quote />);
+        const result = screen.getByText(new RegExp(text));
+        expect(result).toBeInTheDocument();
+    });
+
+    it('should contain an expected author', () => {
+        render(<Quote />);
+        const result = screen.getByText(new RegExp(author));
+        expect(result).toBeInTheDocument();
+    })
+}); 
